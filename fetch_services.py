6d88955e66edf8f51c3313b78404e6d83454e322
valmsmith39a <valmsmith39a@gmail.com>
2021-09-23 23:22:33 -0700

    add query params for crypto symbol

--- fetch_services.py
+++ fetch_services.py
@@ -1,15 +1,17 @@
 import os
 import requests
-DEFAULT_SEARCH_TERM='BTC'
 
-def fetch_tweets(search_term=DEFAULT_SEARCH_TERM):
+DEFAULT_SYMBOL = "BTC"
+
+def fetch_tweets(symbol=DEFAULT_SYMBOL):
 	BEARER_TOKEN = os.getenv('BEARER_TOKEN')
 	auth_token = "Bearer" + " " + BEARER_TOKEN
 	headers_auth = {"Authorization": auth_token}
 	base_url = "https://api.twitter.com/2/tweets/search/recent"
-	query_params = "?query=" + search_term + "&max_results=100"
+	query_params = "?query=" + symbol + "&max_results=100"
 	URL = base_url + query_params
-	# https://api.twitter.com/2/tweets/search/recent?query=btc'
+
+	# Ex url: https://api.twitter.com/2/tweets/search/recent?query=btc'
 	r = requests.get(URL, headers=headers_auth)
 	return r.json()
 	
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fetch_services import fetch_tweets
 
@@ -7,7 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "hello metaverse"}
 
-@app.get("/tweets/all")
-async def get_tweets_all():
-    all_tweets = fetch_tweets()
+@app.get("/crypto-tweets")
+async def get_tweets_all(symbol: Optional[str]):
+    all_tweets = fetch_tweets(symbol)
     return all_tweets
